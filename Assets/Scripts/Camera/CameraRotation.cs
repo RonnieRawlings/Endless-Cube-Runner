99aebfda49b716f17a9506daab1cd21a852f6665
Ronnie Rawlings <rr258736@falmouth.ac.uk>
2023-08-19 23:43:25 +0100

    Implemented/Adjusted - Camera Rotation + Camera Pos
    This commit implements rotation for the camera when the player is moving. The camera's position & starting rotation has also been adjusted to give the player a better view of themseleves and the oncoming obstacles.
    
    Next, add different coloured blocks that start to appear the longer the player goes on.

---
+++ Assets/Scripts/Camera/CameraRotation.cs
@@ -0,0 +1,46 @@
+// Author - Ronnie Rawlings.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    private Quaternion cameraStartRot;
+
+    /// <summary> method <c>ChangeCameraRotation</c> Checks if the player is moving, adjusts camera rotation to fit movement. </summary>
+    public void ChangeCameraRotation()
+    {
+        Quaternion targetRotation;
+        float lerpSpeed = 5f; // Increase this value to make the rotation faster
+        if (Input.GetKey(KeyCode.A))
+        {
+            targetRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 10f);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            targetRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, -10f);
+        }
+        else
+        {
+            targetRotation = cameraStartRot;
+            lerpSpeed = 5f; // Use a higher speed when returning to the starting rotation
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * lerpSpeed);
+    }
+
+
+
+    // Called on script initlization.
+    void Awake()
+    {
+        cameraStartRot = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeCameraRotation();
+    }      
+}
